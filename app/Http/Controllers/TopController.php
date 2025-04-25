9858fe25a6f21afc56728bcb787928f395667586
morizodisco <8wakawo@gmail.com>
2025-04-25 13:13:08 +0900

    update : 登録サイト一覧表示追加

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -1,11 +1,16 @@
 <?php
 namespace App\Http\Controllers;
+use App\Models\Site;
 use Inertia\Inertia;
 
 class TopController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Top');
+        $sites = Site::all();
+
+        return Inertia::render('Top', [
+            'site_list' => $sites
+        ]);
     }
 }
--- resources/js/Pages/Top.vue
+++ resources/js/Pages/Top.vue
@@ -1,28 +1,28 @@
 <script setup>
 import { ref } from 'vue'
 import axios from 'axios'
+import Header from "@/Components/Header.vue";
+
+const { site_list } = defineProps({
+    site_list: Array,
+})
 
 const file = ref(null)
 const result = ref(null)
-const imageUrl = ref('')
 
-const onFileChange = (e) => {
+const saveURL = (e) => {
     file.value = e.target.files[0]
 }
 
-const uploadFile = async () => {
+const uploadURL = async () => {
     if (!file.value) return
 
     const formData = new FormData()
     formData.append('file', file.value)
 
     try {
-        const res = await axios.post('/api/upload/image', formData)
+        await axios.post('/api/upload/image', formData)
         result.value ='アップロード成功'
-        imageUrl.value = res.data.result.variants[0].replace(
-            'https://imagedelivery.net',
-            'https://ad-rip.net/cdn-cgi/imagedelivery'
-        )
     } catch (e) {
         result.value = 'アップロード失敗'
     }
@@ -32,11 +32,30 @@ const uploadFile = async () => {
 <template>
     <Header />
     <div id="main_wrap">
-        <input type="file" @change="onFileChange" />
-        <button @click="uploadFile">アップロード</button>
-        <p v-if="result">{{ result }}</p>
-        <div v-if="imageUrl">
-            <img :src="imageUrl" alt="アップロードされた画像" />
+        <div class="register_area">
+            <input type="text" @change="saveURL" />
+            <button @click="uploadURL">登録</button>
+            <p v-if="result">{{ result }}</p>
+        </div>
+        <div class="site_list">
+            <h2>登録サイト一覧</h2>
+            <table>
+                <tr>
+                    <th>サイト名</th>
+                    <th>サイトURL</th>
+                    <th>登録日時</th>
+                    <th>操作</th>
+                </tr>
+                <tr v-for="site in site_list">
+                    <th>{{ site.name }}</th>
+                    <th>{{ site.url }}</th>
+                    <th>{{ site.created_at }}</th>
+                    <th>
+                        <button>編集</button>
+                        <button>停止</button>
+                    </th>
+                </tr>
+            </table>
         </div>
     </div>
 </template>
@@ -46,5 +65,37 @@ const uploadFile = async () => {
     max-width: 1020px;
     margin: auto;
     padding: 40px 80px;
+
+    input{
+        border: 1px solid #E0E0E0;
+        border-radius: 50px;
+    }
+
+    button{
+        background: #0f0f0f;
+        width: 100px;
+        padding: 10px;
+        color: #fff;
+        border-radius: 50px;
+    }
+
+    .site_list{
+        border: 1px solid #E0E0E0;
+        border-radius: 8px;
+        margin-top: 30px;
+
+        h2{
+            padding: 15px;
+            font-weight: bold;
+        }
+
+        table{
+            width: 100%;
+
+            tr{
+                border-bottom: 1px solid #E0E0E0;
+            }
+        }
+    }
 }
 </style>
