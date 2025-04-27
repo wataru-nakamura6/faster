2f7396bec5dd6617bb6dc529fd59d94e3ff3da56
wataru-nakamura6 <cocoa2647@gmail.com>
2025-04-27 20:56:07 +0900

    update: DynamoDBへのデータ登録処理追加

--- app/Http/Controllers/ScrapeController.php
+++ app/Http/Controllers/ScrapeController.php
@@ -2,18 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\DynamoDbService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Aws\DynamoDb\Marshaler;
 
 class ScrapeController extends Controller
 {
+    protected $dynamoDb;
+
+    public function __construct(DynamoDbService $dynamoDb)
+    {
+        $this->dynamoDb = $dynamoDb;
+    }
+
     /**
      * URLへ画像のスクレイピング→CloudFlare保存
      */
-    public function fetchImages()
+    public function fetchImages(Request $request)
     {
+        // TODO: 画像の重複登録を避ける処理
 //        $url = $request->input('url');
-        // 開発用
+        // 開発用URL
         $url = 'https://www.leggings-rank.info';
 
         if (!$url) {
@@ -29,9 +39,13 @@ class ScrapeController extends Controller
                 return response()->json(['error' => 'スクレイパーAPIからの応答に失敗しました'], 500);
             }
 
-            dd(response()->json($response->json()));
+            $items = $response->json();
+            // TODO: 25件以上の場合のループ処理
+            $this->dynamoDb->batchPutItems('images', $items);
+            return $items;
+
         } catch (\Exception $e) {
-            dd(response()->json(['error' => 'エラー: ' . $e->getMessage()], 500));
+            return response()->json(['error' => 'エラー: ' . $e->getMessage()], 500);
         }
     }
 }
---
+++ app/Services/DynamoDbService.php
@@ -0,0 +1,81 @@
+<?php
+namespace App\Services;
+
+use Aws\DynamoDb\DynamoDbClient;
+use Aws\DynamoDb\Marshaler;
+
+class DynamoDbService
+{
+    protected DynamoDbClient $client;
+    protected Marshaler $marshaler;
+
+    /**
+     * DynamoDB接続
+     */
+    public function __construct()
+    {
+        $this->client = new DynamoDbClient([
+            'region' => env('AWS_DEFAULT_REGION'),
+            'version' => 'latest',
+            'credentials' => [
+                'key'    => env('AWS_ACCESS_KEY_ID'),
+                'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            ],
+            'endpoint' => env('AWS_DYNAMODB_ENDPOINT'),
+        ]);
+
+        $this->marshaler = new Marshaler();
+    }
+
+    /**
+     * レコード追加
+     */
+    public function putItem(string $tableName, array $item)
+    {
+        $params = [
+            'TableName' => $tableName,
+            'Item' => $this->marshaler->marshalItem($item),
+        ];
+
+        return $this->client->putItem($params);
+    }
+
+    /**
+     * レコード取得
+     */
+    public function getItem(string $tableName, array $key)
+    {
+        $params = [
+            'TableName' => $tableName,
+            'Key' => $this->marshaler->marshalItem($key),
+        ];
+
+        $result = $this->client->getItem($params);
+
+        return $this->marshaler->unmarshalItem($result->get('Item'));
+    }
+
+    /**
+     * 複数レコード追加 (最大25件まで)
+     */
+    public function batchPutItems(string $tableName, array $items)
+    {
+        $requests = [];
+
+        foreach ($items as $item) {
+            $requests[] = [
+                'PutRequest' => [
+                    'Item' => $this->marshaler->marshalItem($item),
+                ],
+            ];
+        }
+
+        $params = [
+            'RequestItems' => [
+                $tableName => $requests,
+            ],
+        ];
+
+        return $this->client->batchWriteItem($params);
+    }
+}
