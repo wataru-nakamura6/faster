6ead2fbb3e13224341902024248128f644eaa8c7
wataru-nakamura6 <cocoa2647@gmail.com>
2025-04-27 15:00:12 +0900

    tests: スクレイピングAPI結合テストコード仮作成

---
+++ tests/Feature/FetchImagesTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Http;
+use Tests\TestCase;
+
+class FetchImagesTest extends TestCase
+{
+    public function test_正常に画像を取得できる()
+    {
+        $this->withoutExceptionHandling(); // エラー内容を見たい場合
+
+        // テスト対象のURL
+        $url = 'https://www.leggings-rank.info';
+
+        // /scrape に POST リクエストを送る
+        $response = $this->postJson('/scrape', [
+            'url' => $url,
+        ]);
+
+        // 結果の確認
+        $response->assertStatus(200); // 200が返るか？
+        $response->assertJsonStructure([
+            'images', // imagesキーが存在するか？
+        ]);
+    }
+}
